a931e5346a166babd691eaefb17337fc0e9b21c9
kirya617 <kirill.vagner@mail.ru>
2020-10-13 23:16:31 +0700

    Version 2.1: Error correction. Analysis of identical letters

--- func.c
+++ func.c
@@ -2,16 +2,16 @@
 
 void hangman(int n){ //функция выводящая на экран изображение виселицы
     switch (n) {
-    case 9: printf("  ____ |\n |   | |\n |   O |\n |  /|\\|\n_|_ / \\|  "); break;    
-    case 8: printf("  ____ |\n |   | |\n |   O |\n |  /|\\|\n_|_ /  |  "); break;
-    case 7: printf("  ____ |\n |   | |\n |   O |\n |  /|\\|\n_|_    |  "); break;
-    case 6: printf("  ____ |\n |   | |\n |   O |\n |  /| |\n_|_    |  "); break;
-    case 5: printf("  ____ |\n |   | |\n |   O |\n |   | |\n_|_    |  "); break;
-    case 4: printf("  ____ |\n |   | |\n |   O |\n |     |\n_|_    |  "); break;
-    case 3: printf("  ____ |\n |     |\n |     |\n |     |\n_|_    |  "); break;
-    case 2: printf("       |\n |     |\n |     |\n |     |\n_|_    |  "); break;
-    case 1: printf("       |\n       |\n       |\n       |\n___    |  "); break;
-    case 0: printf("       |\n       |\n       |\n       |\n       |  "); break;
+    case 9: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |  / \\ |\n_|_     | "); break;    
+    case 8: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |  /   |\n_|_     | "); break;
+    case 7: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |      |\n_|_     | "); break;
+    case 6: printf("  ____  |\n |   |  |\n |   O  |\n |  /|  |\n |      |\n_|_     | "); break;
+    case 5: printf("  ____  |\n |   |  |\n |   O  |\n |   |  |\n |      |\n_|_     | "); break;
+    case 4: printf("  ____  |\n |   |  |\n |   O  |\n |      |\n |      |\n_|_     | "); break;
+    case 3: printf("  ____  |\n |      |\n |      |\n |      |\n |      |\n_|_     | "); break;
+    case 2: printf("        |\n |      |\n |      |\n |      |\n |      |\n_|_     | "); break;
+    case 1: printf("        |\n        |\n        |\n        |\n        |\n___     | "); break;
+    case 0: printf("        |\n        |\n        |\n        |\n        |\n        | "); break;
     }
 }
 
--- main.c
+++ main.c
@@ -8,25 +8,24 @@ int main(){
         return 1;
     }
     int sizeFile = 0, sizeWord, stayed_char, i, k = 0, err = 0, count = 0;
-    char buffer[40], word[40] = {0}, *keyword, symbols[20] = {0}, symbol;
-    while (fscanf(words, "%s", buffer) != EOF)
+    char word[20] = {0}, symbols[27] = {0}, symbol;
+    while (fscanf(words, "%s", word) != EOF)
         sizeFile++;
     fseek(words, 0, SEEK_SET);
-    for(i = 0; i < getrand(0, sizeFile); i++)
+    sizeFile = getrand(0, sizeFile);
+    for(i = 0; i < sizeFile; i++)
         fscanf(words, "%s", word);
     fclose(words);
 
     sizeWord = length_string(word);
     stayed_char = sizeWord;
-//    keyword = (char *)malloc(sizeWord);
+
     char *unknown_Word = calloc(sizeWord, sizeof(char));
-    for (i = 0; i < sizeWord; i++){
+    for (i = 0; i < sizeWord; i++)
         unknown_Word[i] = '_';
-//        keyword[i] = word[i];
-    }    
     
     while (stayed_char >= 0 && err <= 9){
-//        system("clear");
+        system("clear");
         if (k == 1)
             err++;
         hangman(err);
@@ -37,8 +36,14 @@ int main(){
             free(unknown_Word);
             return 0;
         }
+        if (stayed_char == 0){
+            printf("%s", word);
+            printf("\nВы победили!\n");
+            free(unknown_Word);
+            return 0;
+        }
 
-        printf("%s\n", unknown_Word);
+        printf(" %s\n", unknown_Word);
         printf("Введенные буквы: %s\n", symbols);
         printf("Введите букву: ");
         scanf("%s", &symbol);
@@ -50,14 +55,17 @@ int main(){
                 unknown_Word[i] = word[i];
             }
         }
-        symbols[count++] = symbol;
-        
+        for (i = 0; i < count;){
+            if (symbols[i] == symbol){
+                k = 0;
+                break;
+            }
+            else
+                i++;
+        }
+        if ( i == count)
+            symbols[count++] = symbol;
     }
-    
-    
-
-
-
     return 0;
 }
 
