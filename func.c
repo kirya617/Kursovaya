b8855fd97c7c54f3d511d1b3b61f6cb62d4f4266
kirya617 <kirill.vagner@mail.ru>
2020-11-08 22:45:15 +0700

    Add clang-format

--- func.c
+++ func.c
@@ -1,18 +1,22 @@
 #include "func.h"
 
-void hangman(int n){ //функция выводящая на экран изображение виселицы
-    switch (n) {
-    case 9: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |  / \\ |\n_|_     | "); break;    
-    case 8: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |  /   |\n_|_     | "); break;
-    case 7: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |      |\n_|_     | "); break;
-    case 6: printf("  ____  |\n |   |  |\n |   O  |\n |  /|  |\n |      |\n_|_     | "); break;
-    case 5: printf("  ____  |\n |   |  |\n |   O  |\n |   |  |\n |      |\n_|_     | "); break;
-    case 4: printf("  ____  |\n |   |  |\n |   O  |\n |      |\n |      |\n_|_     | "); break;
-    case 3: printf("  ____  |\n |      |\n |      |\n |      |\n |      |\n_|_     | "); break;
-    case 2: printf("        |\n |      |\n |      |\n |      |\n |      |\n_|_     | "); break;
-    case 1: printf("        |\n        |\n        |\n        |\n        |\n___     | "); break;
-    case 0: printf("        |\n        |\n        |\n        |\n        |\n        | "); break;
+int hangman(int n){ //функция выводящая на экран изображение виселицы
+    if ((n <= 9) && (n >= 0)){
+        switch (n) {
+            case 9: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |  / \\ |\n_|_     | "); break;    
+            case 8: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |  /   |\n_|_     | "); break;
+            case 7: printf("  ____  |\n |   |  |\n |   O  |\n |  /|\\ |\n |      |\n_|_     | "); break;
+            case 6: printf("  ____  |\n |   |  |\n |   O  |\n |  /|  |\n |      |\n_|_     | "); break;
+            case 5: printf("  ____  |\n |   |  |\n |   O  |\n |   |  |\n |      |\n_|_     | "); break;
+            case 4: printf("  ____  |\n |   |  |\n |   O  |\n |      |\n |      |\n_|_     | "); break;
+            case 3: printf("  ____  |\n |      |\n |      |\n |      |\n |      |\n_|_     | "); break;
+            case 2: printf("        |\n |      |\n |      |\n |      |\n |      |\n_|_     | "); break;
+            case 1: printf("        |\n        |\n        |\n        |\n        |\n___     | "); break;
+            case 0: printf("        |\n        |\n        |\n        |\n        |\n        | "); break;
+        }
+        return 0;
     }
+    return 1;
 }
 
 int getrand(int min, int max) //Функция генерации случайных чисел
